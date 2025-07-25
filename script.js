311c04b8907079aa0d1e6fec96fa658a52098baa
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-07-25 17:18:06 +0300

    updated js

--- script.js
+++ script.js
@@ -2,4 +2,20 @@ const joke = document.getElementById('joke');
 const btn = document.getElementById('jokeBtn');
 
 generateJoke();
-async function generateJoke(params) {}
+
+async function generateJoke() {
+	const config = {
+		headers: {
+			Accept: 'application/json',
+		},
+	};
+	try {
+		const res = await fetch('https://icanhazdadjoke.com', config);
+		const data = await res.json();
+		joke.innerHTML = data.joke;
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+btn.addEventListener('click', generateJoke);
